8d5d4d8a8895f85d4524a476725f353770737df9
plz717 <panglinzhuo@gmail.com>
2016-12-01 11:08:50 +0800

    add get-lantern.sh

---
+++ 妈妈在也不用担心我重装系统啦啦啦/getlantern.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+wget https://raw.githubusercontent.com/getlantern/lantern-binaries/master/lantern-installer-beta-64-bit.deb
+dpkg -i lantern-installer-beta-64-bit.deb
+rm -f lantern-installer-beta-64-bit.deb
+#lantern &
+#bg 1
+#exit
+
